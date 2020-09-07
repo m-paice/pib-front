82346f279ccc19a152d7ec0be7374c741cfd4039
Matheus Paice <matheus.paice@ikatec.com.br>
2020-09-07 14:31:05 -0300

    fix(refactor):corrige service api

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api = axios.create({
+export default axios.create({
     baseURL: process.env.API_URL || "http://127.0.0.1:3333",
     headers: {
         "Content-Type": "application/json",
