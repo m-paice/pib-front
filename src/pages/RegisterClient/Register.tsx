0aaf9aa4571a9f7e32122a35158ad667f8e880de
Matheus Paice <matheus.paice@ikatec.com.br>
2021-01-22 14:30:11 -0300

    fix(registro): corrige box

--- src/pages/RegisterClient/Register.tsx
+++ src/pages/RegisterClient/Register.tsx
@@ -20,17 +20,29 @@ const Register: React.FC<Props> = ({}) => {
 
     return (
         <div className="page">
-            <div className="container meu-cadastro" style={{ maxWidth: 500 }}>
+            <div
+                className="container meu-cadastro"
+                style={{
+                    maxWidth: 500,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "column",
+                }}
+            >
                 <div className="descmod cadastro">
-                    <div className="titulo-mob noneBr">Verifique seu documento</div>
+                    <div className="titulo-mob noneBr" style={{ textAlign: "center" }}>
+                        Verifique seu documento
+                    </div>
                 </div>
                 <input
                     className="form-control"
+                    placeholder="Digite seu CPF ou CNPJ"
                     value={value}
                     onChange={(event: React.ChangeEvent<HTMLInputElement>) => setValue(event.target.value)}
                 />
 
-                <button style={{ marginTop: 50 }} type="button" onClick={handleSetPageRegister}>
+                <button style={{ marginTop: 50 }} className="btpadrao" type="button" onClick={handleSetPageRegister}>
                     verificar
                 </button>
             </div>
