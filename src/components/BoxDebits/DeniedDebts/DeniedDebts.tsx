48b1a8acbfc76a314b475d7691de76f48b415478
Matheus Paice <matheus.paice@ikatec.com.br>
2021-02-10 00:36:56 -0300

    fix(painel): corrige layout mobile p/ negociacoes

--- src/components/BoxDebits/DeniedDebts/DeniedDebts.tsx
+++ src/components/BoxDebits/DeniedDebts/DeniedDebts.tsx
@@ -79,85 +79,151 @@ const DeniedDebts: React.FC<Props> = (props) => {
     };
 
     return (
-        <div className="cada debito">
-            <div className="row rowCenter">
-                <div className="col-md-1">
-                    <a href="javascript:void(0);" className="smais">
-                        {info ? (
-                            <Dash className="btn glyphicon" onClick={() => handleSetState("info", !info)} />
-                        ) : (
-                            <Plus className="btn glyphicon" onClick={() => handleSetState("info", !info)} />
-                        )}
-                    </a>
-                </div>
-                <div className=" col-md-2">
-                    <span className="labelDebito text-left">{lojista.usuario.nome}</span>
-                </div>
-                <div className="col-md-7 d-flex justify-content-between">
-                    <div style={{ width: 75 }}>
-                        <div className="lab">
-                            <div className="help" data-toggle="tooltip" data-placement="top" title="Texto ajuda">
-                                Tipo
-                                <span className="glyphicon glyphicon-question-sign ml-1"></span>
+        <>
+            <div className="web">
+                <div className="cada debito">
+                    <div className="row rowCenter">
+                        <div className="col-md-1">
+                            <a href="javascript:void(0);" className="smais">
+                                {info ? (
+                                    <Dash className="btn glyphicon" onClick={() => handleSetState("info", !info)} />
+                                ) : (
+                                    <Plus className="btn glyphicon" onClick={() => handleSetState("info", !info)} />
+                                )}
+                            </a>
+                        </div>
+                        <div className=" col-md-2">
+                            <span className="labelDebito text-left">{lojista.usuario.nome}</span>
+                        </div>
+                        <div className="col-md-7 d-flex justify-content-between">
+                            <div style={{ width: 75 }}>
+                                <div className="lab">
+                                    <div
+                                        className="help"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Texto ajuda"
+                                    >
+                                        Tipo
+                                        <span className="glyphicon glyphicon-question-sign ml-1"></span>
+                                    </div>
+                                </div>
+                                <div className="txt-12 m-0 font-weight-bold"> {tiposDocumentos[tipoDoc]} </div>
+                            </div>
+                            <div className="div c"></div>
+                            <div>
+                                <div className="lab">
+                                    <div
+                                        className="help"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Texto ajuda"
+                                    >
+                                        Vencimento
+                                        <span className="glyphicon glyphicon-question-sign ml-1"></span>
+                                    </div>
+                                </div>
+                                <div className="txt-12 m-0 font-weight-bold"> {formatDate(new Date(vencimento))}</div>
+                            </div>
+                            <div className="div c"></div>
+                            <div>
+                                <div className="lab">
+                                    <div
+                                        className="help"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Este é um texto de ajuda com número de caracteres elevado."
+                                    >
+                                        Valor da dívida
+                                        <span className="glyphicon glyphicon-question-sign ml-1"></span>
+                                    </div>
+                                </div>
+                                <div className="txt-12 m-0 font-weight-bold">{formatPrice(Number(valor))}</div>
+                            </div>
+                            <div className="div c"></div>
+                            <div>
+                                <div className="lab">
+                                    <div
+                                        className="help"
+                                        data-toggle="tooltip"
+                                        data-placement="top"
+                                        title="Texto ajuda"
+                                    >
+                                        Registro
+                                        <span className="glyphicon glyphicon-question-sign ml-1"></span>
+                                    </div>
+                                </div>
+                                <div className="txt-12 m-0 font-weight-bold">{formatDate(new Date(inclusao))}</div>
                             </div>
                         </div>
-                        <div className="txt-12 m-0 font-weight-bold"> {tiposDocumentos[tipoDoc]} </div>
-                    </div>
-                    <div className="div c"></div>
-                    <div>
-                        <div className="lab">
-                            <div className="help" data-toggle="tooltip" data-placement="top" title="Texto ajuda">
-                                Vencimento
-                                <span className="glyphicon glyphicon-question-sign ml-1"></span>
-                            </div>
+                        <div className=" col-md-2 cb ">
+                            {!lojista.usuario.habilitado ? (
+                                <a onClick={() => handleSetState("info", !info)} className="btneg gray btn">
+                                    Contate o credor
+                                </a>
+                            ) : (
+                                <a onClick={() => handleSetState("negociation", !negociation)} className="btneg btn">
+                                    Quero Negociar
+                                </a>
+                            )}
                         </div>
-                        <div className="txt-12 m-0 font-weight-bold"> {formatDate(new Date(vencimento))}</div>
                     </div>
-                    <div className="div c"></div>
-                    <div>
-                        <div className="lab">
-                            <div
-                                className="help"
-                                data-toggle="tooltip"
-                                data-placement="top"
-                                title="Este é um texto de ajuda com número de caracteres elevado."
-                            >
-                                Valor da dívida
-                                <span className="glyphicon glyphicon-question-sign ml-1"></span>
-                            </div>
-                        </div>
-                        <div className="txt-12 m-0 font-weight-bold">{formatPrice(Number(valor))}</div>
-                    </div>
-                    <div className="div c"></div>
-                    <div>
-                        <div className="lab">
-                            <div className="help" data-toggle="tooltip" data-placement="top" title="Texto ajuda">
-                                Registro
-                                <span className="glyphicon glyphicon-question-sign ml-1"></span>
-                            </div>
-                        </div>
-                        <div className="txt-12 m-0 font-weight-bold">{formatDate(new Date(inclusao))}</div>
-                    </div>
-                </div>
-                <div className=" col-md-2 cb ">
-                    {!lojista.usuario.habilitado ? (
-                        <a onClick={() => handleSetState("info", !info)} className="btneg gray btn">
-                            Contate o credor
-                        </a>
-                    ) : (
-                        <a onClick={() => handleSetState("negociation", !negociation)} className="btneg btn">
-                            Quero Negociar
-                        </a>
+
+                    {negociation && (
+                        <Negociation
+                            debit={Number(valor)}
+                            monthForRule={amountMonth}
+                            lojistaId={lojista.id}
+                            debitoId={id}
+                        />
                     )}
+
+                    {info && <MoreInfo {...lojista} />}
                 </div>
             </div>
+            <div className="mobile">
+                <div className="p-2 cada debito">
+                    <div className="d-flex align-items-center justify-content-between">
+                        <div className="d-flex align-items-center ">
+                            <a className="smais">
+                                {info ? (
+                                    <Dash className="btn glyphicon" onClick={() => handleSetState("info", !info)} />
+                                ) : (
+                                    <Plus className="btn glyphicon" onClick={() => handleSetState("info", !info)} />
+                                )}
+                            </a>
 
-            {negociation && (
-                <Negociation debit={Number(valor)} monthForRule={amountMonth} lojistaId={lojista.id} debitoId={id} />
-            )}
+                            <span className="ml-2 labelDebito text-left">{lojista.usuario.nome}</span>
+                        </div>
+                        <div className="width180">
+                            {!lojista.usuario.habilitado ? (
+                                <a onClick={() => handleSetState("info", !info)} className=" btneg gray btn">
+                                    Contate o credor
+                                </a>
+                            ) : (
+                                <a
+                                    onClick={() => handleSetState("negociation", !negociation)}
+                                    className="width180 btneg btn"
+                                >
+                                    Quero Negociar
+                                </a>
+                            )}
+                        </div>
+                    </div>
 
-            {info && <MoreInfo {...lojista} />}
-        </div>
+                    {negociation && (
+                        <Negociation
+                            debit={Number(valor)}
+                            monthForRule={amountMonth}
+                            lojistaId={lojista.id}
+                            debitoId={id}
+                        />
+                    )}
+
+                    {info && <MoreInfo {...lojista} />}
+                </div>
+            </div>
+        </>
     );
 };
 
